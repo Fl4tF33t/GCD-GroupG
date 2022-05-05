905b2b68746d6cc5bc9faa63fd832d7e799be62f
ab0214 <ab0214@student.jamk.fi>
2022-05-05 21:16:50 +0300

    added post processing to player 2

---
+++ Assets/Scripts/Player2PPScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+public class Player2PPScript : MonoBehaviour
+{
+    [SerializeField] private PostProcessVolume ElectrPPVol;
+    [SerializeField] private PostProcessVolume HealthPPVol;
+
+    private void Start()
+    {
+        Level1GameManager.Instance.health.OnValueChanged += OnValueChanged;
+    }
+
+    void OnValueChanged(int previous, int current)
+    {
+        HealthPPVol.weight = (float)(100 - current) / 100;
+        StartCoroutine("electrEffect");
+    }
+
+    private IEnumerator electrEffect()
+    {
+        ElectrPPVol.weight = 1f;
+        yield return new WaitForSeconds(0.2f);
+        ElectrPPVol.weight = 0f;
+    }
+}
