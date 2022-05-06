947688d6d336855d44798b292a4158c1aa52aba2
ab0214 <ab0214@student.jamk.fi>
2022-05-06 13:46:44 +0300

    correct answer hint in player2 room

--- Assets/Scripts/TextEffect.cs
+++ Assets/Scripts/TextEffect.cs
@@ -3,36 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-// To do: method that reveals correct letters (slowly or randomly or one-by-one, etc.)
-
 public class TextEffect : MonoBehaviour
 {
-    public string text; // The correct word that players try to solve. This is public for now, because other scripts will set/get this.
-    private int length;
+    public string answer; // The correct word that players try to solve. This is public for now, because other scripts will set/get this.
+    private int length; // length of answer and randomWord
     private TextMeshPro tmp;
-    string randomText = "";
+    private char[] randomWord;
+    private bool revealAnswer = false;
 
     void Start()
     {
         tmp = GetComponent<TextMeshPro>();
-        length = text.Length;
+        length = answer.Length;
 
-        // generates a random string with the same length as the answer
+        // generates a random character array with the same length as the answer
+        randomWord = new char[length];
         for (int i = 0; i < length; i++) {
-            string s = ((char)Random.Range(0x41, 0x5A)).ToString(); // random uppercase letter
-            randomText = randomText.Insert(i, s);
-            randomText.Insert(i, s);
+            char c = ((char)Random.Range(0x41, 0x5A)); // random uppercase letter
+            randomWord[i] = c;
         }
-        tmp.text = randomText;
+        tmp.text = randomWord.ToString();
+
+        StartCoroutine("ChangeRandomLetter");
+        Level1GameManager.Instance.health.OnValueChanged += OnValueChangedTextEffect;
     }
 
-    void Update()
+    private void OnValueChangedTextEffect(int previous, int current)
+    {
+        StartCoroutine("RevealAnswer");
+    }
+
+    private IEnumerator ChangeRandomLetter()
     {
         // Replaces a character at a random position with a new random letter.
         int i = Random.Range(0, length); // index of the random character to be replaced
-        string s = ((char)Random.Range(0x41, 0x5A)).ToString(); // random uppercase letter
-        randomText = randomText.Insert(i, s);
-        randomText = randomText.Remove(i + 1, 1);
-        tmp.text = "<mspace=1>" + randomText + " </mspace>"; // monospace
+        char c;
+        if (revealAnswer) c = answer[i]; // correct character at the index
+        else { c = ((char)Random.Range(0x41, 0x5A)); } // random uppercase letter
+        randomWord[i] = c;
+
+        // parse output
+        string output = "<mspace=1>"; // monospace
+        if (revealAnswer) // add red color to correct letters if player2 is being electrocuted
+        {
+            for (int j = 0; j < length; j++)
+            {
+                if (randomWord[j] == answer[j]) output += "<color=#FF0000>";
+                output += randomWord[j];
+                if (randomWord[j] == answer[j]) output += "</color>";
+            }
+        }
+        else
+        {
+            output += new string(randomWord); // convert char[] randomWord to string and add to string output
+        }
+        output += "</mspace>"; // monospace
+
+        tmp.text = output;
+
+        yield return new WaitForSeconds(0.05f);
+        StartCoroutine("ChangeRandomLetter");
+    }
+
+    private IEnumerator RevealAnswer()
+    {
+        revealAnswer = true;
+        yield return new WaitForSeconds(0.25f);
+        revealAnswer = false;
+
     }
 }
